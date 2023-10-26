4676c844a92e2d82d1f5927b3a38b8746b03e781
ZugravuPaul <zugravupaul666@yahoo.com>
2023-10-26 10:02:18 +0300

    Add project files.

---
+++ EasyRent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace EasyRent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User() { Username="test", Email="test" , Password= "test", PhoneNumber="test", Role="ADMIN"};
+
+            DBDataContext db = new DBDataContext();
+            db.
+        }
+    }
+}
