d58e0d20665b825623449038748ee4f93d407799
ZugravuPaul <zugravupaul666@yahoo.com>
2023-10-30 09:05:36 +0200

    login page

--- EasyRentWin/MainWindow.xaml.cs
+++ EasyRentWin/MainWindow.xaml.cs
@@ -20,15 +20,32 @@ namespace EasyRentWin
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
+                
         }
+        EasyRentDBDataContext db=new EasyRentDBDataContext(Properties.Settings.Default.EasyRentDataBaseConnectionString);
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            EasyRentDBDataContext dBDataContext = new EasyRentDBDataContext();
-            dBDataContext.SubmitChanges();
+            User user = new User();
+            user.Username = txtUsername.Text;
+            user.Password = passPassword.Password;
+            user.Email = txtEmail.Text;
+            user.PhoneNumber = txtPhone.Text;
+            user.Role = "ADMIN";
+
+            if (string.IsNullOrWhiteSpace(user.Username) || string.IsNullOrWhiteSpace(user.Password) || string.IsNullOrWhiteSpace(user.Email) || string.IsNullOrWhiteSpace(user.PhoneNumber))
+            {
+                MessageBox.Show("Campuri necompletate");
+                return;
+            }
+
+            db.Users.InsertOnSubmit(user);
+            db.SubmitChanges();
+            MessageBox.Show("Bravo tati...");
         }
     }
 }
